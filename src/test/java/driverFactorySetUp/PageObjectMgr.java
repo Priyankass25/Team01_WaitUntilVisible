10f642cfd1eb9978a3713c36ebce45a5bca41a44
sowmya <sowmya12345g@gmail.com>
2026-08-08 01:23:33 -0500

    added pom

--- src/test/java/driverFactorySetUp/PageObjectMgr.java
+++ src/test/java/driverFactorySetUp/PageObjectMgr.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
 import pageObjects.ProgramPage;
+import pageObjects.ProgramPage2;
 
 public class PageObjectMgr {
 
@@ -12,6 +13,7 @@ public class PageObjectMgr {
 	private LoginPage loginPage;
 	private HomePage homePage;
 	private ProgramPage programPage;
+	private ProgramPage2 programPage2;
 	
 	public PageObjectMgr(WebDriver driver) {
 		this.driver = driver;
@@ -35,5 +37,11 @@ public class PageObjectMgr {
 			programPage = new ProgramPage(driver);
 		}
 		return programPage;
+	}
+	public ProgramPage2 getProgramPage2() {
+		if(programPage2 == null) {
+			programPage2 = new ProgramPage2(driver);
+		}
+		return programPage2;
 	}
 }
