a16f29075c538eb18da8530ccf383b06b1dce6db
Priyankass25 <165202728+Priyankass25@users.noreply.github.com>
2026-08-10 23:59:17 -0500

    Update LoginPage.java

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -20,6 +20,16 @@ public class LoginPage {
 	    public By selectRoleDropdown = By.id("mat-select-0");
 	    public By loginBtn = By.id("login");
 	    public By adminOption = By.xpath("//mat-option//span[normalize-space()='Admin']");
+	public By LMStext = By.xpath("//img[@src='assets/img/LMS-logo.jpg']");
+	public By logo = By.xpath("//img[contains(@src, 'logo')]");
+	public By loginmsg = By.xpath("//p[contains(text(),'Please login to LMS application')]");
+	public By usertext = By.xpath("//span[contains(text(),'User')]");
+	public By pwdtext = By.xpath("//span[contains(text(),'Password')]");
+	public By userasterik = By.xpath("//span[contains(text(),'User')]/following::span[@class='mat-placeholder-required mat-form-field-required-marker ng-tns-c159-14 ng-star-inserted']");
+	public By pwdasterik = By.xpath("//span[contains(text(),'Password')]/following::span[contains(text(), ' *')][1]");
+	public By selecttext = By.xpath("//span[@class='ng-tns-c159-16 ng-star-inserted']");
+	public By Optionstaff = By.xpath("//mat-option//span[normalize-space()=' Staff ']");
+	public By Optionstu = By.xpath("//mat-option//span[normalize-space()=' Student ']");
 
 	    public boolean isLoginPageLoaded() {
 	    	return driver.getCurrentUrl().contains("login");
@@ -44,5 +54,80 @@ public class LoginPage {
 		public void clickLoginBtn() {			
 			driver.findElement(loginBtn).click();		
 		}
+		public boolean isDashboardTextDisplayed() {
+		return common.isDisplayed(Dashboard);
+	}
+
+	public void enterinvalidurl() {
+		String invalid_url = ConfigReader.getProperty("invalid_url");
+		driver.get(invalid_url);
+		// LoggerLoad.info("Create Program Name is " +programName);
+
+	}
+
+	public void enterbrokenurl() {
+		String broken_url = ConfigReader.getProperty("broken_url");
+		driver.get(broken_url);
+	}
+
+	public void isLMSTextDisplayed() {
+		boolean LMS = common.isDisplayed(LMStext);
+		Assert.assertTrue(LMS, "LMS header image is displayed on the login page!");
+	}
+
+	public void isCompanynameDisplayed() {
+		String text = common.getText(LMStext);
+		Assert.assertEquals(text, "Numpy Ninja");
+
+	}
+
+	public boolean isLogoDisplayed() {
+		return common.isDisplayed(logo);
+
+	}
+
+	public void isloginmsgDisplayed() {
+		String text = common.getText(loginmsg);
+		Assert.assertEquals(text, "Please login to LMS application");
+	}
+
+	public void textfieldPresence() {
+	        common.isDisplayed(user);
+	}
+
+	public void roledropdown() {
+		driver.findElement(selectRoleDropdown).click();
+		common.waitForVisibility(adminOption);
+
+	}
+	
+	public void textname() {
+		common.isDisplayed(usertext);
 		
+	}
+	public void pwdname() {
+		common.isDisplayed(pwdtext);
+		
+	}
+	public void textnameasterisk() {
+		common.isDisplayed(userasterik);
+		
+	}
+	public void pwdnameasterisk() {
+		common.isDisplayed(pwdasterik);
+		
+	}
+	
+	public void selectPlaceholder() {
+		common.isDisplayed(selecttext);
+		
+	}
+	public void selectPlaceholdervalues() {
+		common.isDisplayed(adminOption);
+		common.isDisplayed(Optionstaff);
+		common.isDisplayed(Optionstu);
+		
+	}
+}
+
 }
